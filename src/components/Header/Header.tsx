554bb65cfadd98fae11afb1a759b0ad47f972e75
Mansi Trivedi <mansitrivedi123123@gmail.com>
2025-08-25 12:42:04 +0530

    fix types

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,7 +8,6 @@ import { usePathname } from "next/navigation";
 
 const Header = () => {
   const pathname = usePathname();
-  console.log("pathname", pathname);
   const [navigationModal, setNavigationModal] = useState(false);
 
   useEffect(() => {
